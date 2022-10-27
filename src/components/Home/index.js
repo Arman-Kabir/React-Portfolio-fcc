f3e78174f1c2aae86abb472ff8e0f8b3ba5c8319
Arman-Kabir <armankabir8@gmail.com>
2022-10-27 08:04:51 +0600

    exploring animate

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 
 const Home = () => {
 
-    const [letterClass,setLetterClass] = useState('text-animate');
+    const [letterClass, setLetterClass] = useState('text-animate');
 
-    const nameArray = [''];
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, <br /> I'm
                     <img src={LogoTitle} alt="developer" />
-                    hovon
+
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray}
+                        idx={15} />
                     <br />
-                    Web developer
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray}
+                        idx={22} />
                 </h1>
                 <h2>Frontend Developer / JavaScript Programmer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
