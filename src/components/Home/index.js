a4178e4930a5411d00b84bdfdb386b87065fbbf0
Arman-Kabir <armankabir8@gmail.com>
2022-11-20 13:40:51 +0600

    few modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
@@ -9,8 +10,14 @@ const Home = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate');
 
-    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-    const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
+    const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return (
         <div className="container home-page">
